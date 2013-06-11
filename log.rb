d1f5ed6583adec3b350c967c8209e898c56018aa
a-munakata <a.munakata@4digit.jp>
2013-06-11 20:35:39 +0900

    PUSH

--- log.rb
+++ log.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database => "./data.sqlite"
+ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database => "./db/data.sqlite"
 
 class Log < ActiveRecord::Base
 	default_scope {order(:timestamp).limit(10)}
