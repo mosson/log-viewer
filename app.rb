b591e15de1488a0874566c5dffccb87d237236de
a-munakata <a.munakata@4digit.jp>
2013-06-11 10:49:50 +0900

    urlのパラメータから環境を取得

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
+require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader' if development?
 require './lib/sinatra/paginate'
 require './log'
-require 'yaml'
 require 'octokit'
+require 'yaml'
+
 
 TARGET_REPO = "a-munakata/sinatra-app"
 
@@ -26,26 +28,16 @@ end
 
 post '/post_issue' do	
 	client = Octokit::Client.new login: ENV["GITHUB_USER"], password: ENV["GITHUB_PASSWORD"]
-	api_response = client.create_issue TARGET_REPO, params[:title], params[:body]
+	api_response = client.create_issue TARGET_REPO, params[:title], params[:body] unless params[:title].nil? && params[:body].nil?
 	redirect api_response.html_url
 end
 
-get "/production" do
-	@production_logs = Log.where(:environment => "production").limit(10).offset(page * 10)
-	@logs_total 		 = Log.where(:environment => "production")	
-	@result 				 = Struct::Result.new(@logs_total.count, @production_logs.count, @production_logs)
-	@title 					 = "PRODUCTION"
+get "/env/:environment" do
+		@logs 					 = Log.where(:environment => params[:environment]).limit(10).offset(page * 10)
+		@logs_total 		 = Log.where(:environment => params[:environment])
+		@result 				 = Struct::Result.new(@logs_total.count, @logs.count, @logs)
 
-	haml :show_logs
-end
-
-get "/staging" do
-	@staging_logs = Log.where(:environment => "staging").limit(10).offset(page * 10).order(:timestamp => "DESC")
-	@logs_total 	= Log.where(:environment => "staging")
-	@result		 		= Struct::Result.new(@logs_total.count, @staging_logs.count, @staging_logs)
-	@title 				= "STAGING"
-	
-	haml :show_logs
+		haml :show_logs
 end
 
 post "/put" do
