843c227d3f3d92f45ff9fbbf421860582d7051ba
a-munakata <a.munakata@4digit.jp>
2013-06-14 20:57:30 +0900

    PUSH

--- db/migrate/migration.rb
+++ db/migrate/migration.rb
@@ -2,16 +2,16 @@ require './log'
 
 class LogStore < ActiveRecord::Migration
 	def self.up
-		create_table 	:logs do |t|			
-			t.integer		:entry_id
+		create_table 	:logs do |t|
 			t.string 		:entry
-			t.integer 	:timestamp
+			t.timestamp	:timestamp
 			t.string		:environment
 			t.integer		:error_status
-			t.boolean		:github
+			t.boolean		:github_issued
+			t.boolean		:open_or_close
 		end
 
-		add_index :logs, [:timestamp, :environment, :error_status], unique: true
+		# add_index :logs, [:timestamp, :environment, :error_status], unique: true
 		
 	end
 end
