7f2687fb2a9f533a61714845dee8567cd6ddafdc
a-munakata <a.munakata@4digit.jp>
2013-06-18 21:16:26 +0900

    attr_accessibleを追加

--- log.rb
+++ log.rb
@@ -2,5 +2,6 @@ require 'active_record'
 	ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database => ENV["DB_PATH"]
 
 class Log < ActiveRecord::Base
+	attr_accessible :entry, :timestamp, :environment, :error_status, :github_issued, :open_or_close
 	default_scope {order(:timestamp)}
 end
