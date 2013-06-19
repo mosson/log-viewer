3955d61c10fccb21041843125690c8a4ed5b3976
a-munakata <a.munakata@4digit.jp>
2013-06-19 18:08:58 +0900

    PUSH

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require './log'
 require 'octokit'
 require 'yaml'
 require 'haml'
+require './lib/paginate'
 
 TARGET_REPO = "a-munakata/log-factory"
 
@@ -31,31 +32,37 @@ post '/issue' do
 	redirect "/production"
 end
 
-get "/invalid" do
+get '/invalid' do
 	"Invalid argument"	
 end
 
-get "/:environment" do
-	@logs = Log.github
-	@logs            = Log.envs(params[:environment]).order(:timestamp => "DESC")
+
+get '/:environment/:page' do
+	@logs            = Log.envs(params[:environment]).limit(10).offset(10 * params[:page].to_i)
 	haml :environment
 end
 
-post "/:environment" do
+
+get '/:environment' do
+	@logs            = Log.envs(params[:environment])
+	haml :environment	
+end
+
+post '/:environment' do
 	@backtrace = params[:backtrace]
 	@environment = params[:environment]
 	@status_code = params[:status_code]	
 	@date_from = params[:date_from]
 	@date_to = params[:date_to]
 
-	@logs = Log.envs(@environment).where(:error_status => @status_code) unless @status_code.nil?	
-	@logs = Log.all.find(:entry => /#{@backtrace}/, :environment => @environment) unless @backtrace.nil?
+	@logs = Log.envs(@environment).where(:error_status => @status_code) unless @status_code.nil?
+	@logs = Log.envs(@environment).where('entry LIKE ?', '%#{@backtrace}%').all unless @backtrace.nil?
 	@logs = Log.envs(@environment).where(:timestamp => @date_from.to_time..@date_to.to_time) unless @date_from.nil? && @date_to.nil?	
 
 	haml :environment
 end
 
 
-get "/env/css/styles.css" do
+get '/env/css/styles.css' do
 	css :stylesheet
 end
