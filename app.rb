1e0e8b1afd544108519d09ee95033a8c3b8156d3
a-munakata <a.munakata@4digit.jp>
2013-06-20 14:43:49 +0900

    ページネーション機能を追加

--- app.rb
+++ app.rb
@@ -36,15 +36,28 @@ get '/invalid' do
 	"Invalid argument"	
 end
 
-
 get '/:environment/:page' do
-	@logs            = Log.envs(params[:environment]).limit(10).offset(10 * params[:page].to_i)
+	redirect "/production" if params[:page] == "production"
+	redirect "/staging" if params[:page] == "staging"
+	@logs    = Log.envs(params[:environment]).limit(10).offset(10 * (params[:page].to_i - 1))
+	
+	paginate = Paginate.new()
+
+	@max_num = Log.envs(params[:environment]).count/10 + 1	
+	@result  = paginate.pages @max_num, 5, params[:page] unless params[:page].nil?
+	
 	haml :environment
 end
 
 
-get '/:environment' do
-	@logs            = Log.envs(params[:environment])
+get '/:environment' do	
+	@logs  = Log.envs(params[:environment]).limit(10).offset(10 * (params[:page].to_i - 1))
+
+	paginate = Paginate.new()
+
+	@max_num = Log.envs(params[:environment]).count/10 + 1
+	@result = paginate.pages @max_num, 5, 1
+		
 	haml :environment	
 end
 
@@ -56,8 +69,8 @@ post '/:environment' do
 	@date_to = params[:date_to]
 
 	@logs = Log.envs(@environment).where(:error_status => @status_code) unless @status_code.nil?
-	@logs = Log.envs(@environment).where('entry LIKE ?', '%#{@backtrace}%').all unless @backtrace.nil?
-	@logs = Log.envs(@environment).where(:timestamp => @date_from.to_time..@date_to.to_time) unless @date_from.nil? && @date_to.nil?	
+	@logs = Log.envs(@environment).where('entry LIKE ?', '%#{@backtrace}%') unless @backtrace.nil?
+	@logs = Log.envs(@environment).where(:timestamp => @date_from.to_time..@date_to.to_time) unless @date_from.nil? && @date_to.nil?
 
 	haml :environment
 end
